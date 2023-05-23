63677545fa14366a006153c5ef10ffdddaf600d6
Gajaralwar <omgajaralwar@teksystems.com>
2023-05-23 21:38:15 +0530

    new deploy3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+const apiKey=environment.apiKey;
+console.log(apiKey)
 export class AppComponent {
   title = 'foodblog';
+  
+
 }
